0a65e5ca0ba6a9c82de26e3ccb28e5b176f4c41a
Andrey Kot <a.kot@geoscan.aero>
2020-09-24 16:41:04 +0300

    fixed settings.py

--- jobs_example/settings.py
+++ jobs_example/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "asdasdd3490rh934hf934f348fh3"
+SECRET_KEY = os.environ['JOBS_EXAMPLE']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
